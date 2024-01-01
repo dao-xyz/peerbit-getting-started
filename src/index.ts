635aa5742ff68c54d2934d870ffcbe71cf6f820f
Marcus Pousette <marcus.pousette@quantleaf.com>
2024-01-01 12:51:40 +0100

    fix: add lint

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,46 @@
 import { field, variant } from "@dao-xyz/borsh";
 import { Program } from "@peerbit/program";
 import { Documents } from "@peerbit/document";
-import { v4 as uuid } from 'uuid';
-
+import { v4 as uuid } from "uuid";
 
 // Abstract document definition we can create many kinds of document types from
-export class BaseDocument { }
-
+export class BaseDocument {}
 
 // We version our documents with a single byte, so in the future, if we want to upgrade our document definition
 // we can do @variant(1) which allows us to store and manage both Documents of type variant 0 and variant 1 in our Document database
 // (This is not needed but recommended)
 @variant(0)
 export class TextDocument extends BaseDocument {
+    @field({ type: "string" })
+    id: string;
 
-	@field({ type: 'string' })
-	id: string
+    @field({ type: "string" })
+    text: string;
 
-	@field({ type: 'string' })
-	text: string
-
-	constructor(text: string) {
-		super()
-		this.id = uuid()
-		this.text = text;
-	}
+    constructor(text: string) {
+        super();
+        this.id = uuid();
+        this.text = text;
+    }
 }
 
 // MyDatabase needs to extends Program so we later can "open" it using the Peerbit client
 @variant("my-database")
 export class MyDatabase extends Program {
+    @field({ type: Documents })
+    documents: Documents<BaseDocument>;
 
+    constructor(properties?: { id?: Uint8Array }) {
+        super();
 
+        // We create an ID field so that the hash of the database/program can be unique defined by this
+        // If this field is omitted calling .open(new MyDataBase()) would yield same address everytime, which is sometimes wanted, sometimes not
 
-	@field({ type: Documents })
-	documents: Documents<BaseDocument>
+        this.documents = new Documents({ id: properties?.id });
+    }
 
-	constructor(properties?: { id?: Uint8Array }) {
-		super()
-
-		// We create an ID field so that the hash of the database/program can be unique defined by this
-		// If this field is omitted calling .open(new MyDataBase()) would yield same address everytime, which is sometimes wanted, sometimes not
-
-		this.documents = new Documents({ id: properties?.id })
-	}
-
-	async open() {
-		// this will be invoked on startup
-		await this.documents.open({ type: BaseDocument, index: { key: 'id' } })
-	}
+    async open() {
+        // this will be invoked on startup
+        await this.documents.open({ type: BaseDocument, index: { key: "id" } });
+    }
 }
-
