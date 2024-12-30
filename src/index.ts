01bac6816986e01cdf5794a7c9b59cc5995e31e2
Marcus Pousette <marcus.pousette@quantleaf.com>
2024-12-30 15:25:32 +0100

    fix: bump pb

--- src/index.ts
+++ src/index.ts
@@ -8,36 +8,38 @@ import { v4 as uuid } from "uuid";
 // (This is not needed but recommended)
 @variant(0)
 export class TextDocument {
+    @field({ type: "string" })
+    id: string;
 
-	@field({ type: "string" })
-	id: string;
+    @field({ type: "string" })
+    text: string;
 
-	@field({ type: "string" })
-	text: string;
-
-	constructor(text: string) {
-		this.id = uuid();
-		this.text = text;
-	}
+    constructor(text: string) {
+        this.id = uuid();
+        this.text = text;
+    }
 }
 
 // MyDatabase needs to extends Program so we later can "open" it using the Peerbit client
 @variant("my-database")
 export class MyDatabase extends Program {
-	@field({ type: Documents })
-	documents: Documents<TextDocument>;
+    @field({ type: Documents })
+    documents: Documents<TextDocument>;
 
-	constructor(properties?: { id?: Uint8Array }) {
-		super();
+    constructor(properties?: { id?: Uint8Array }) {
+        super();
 
-		// We create an ID field so that the hash of the database/program can be unique defined by this
-		// If this field is omitted calling .open(new MyDataBase()) would yield same address everytime, which is sometimes wanted, sometimes not
+        // We create an ID field so that the hash of the database/program can be unique defined by this
+        // If this field is omitted calling .open(new MyDataBase()) would yield same address everytime, which is sometimes wanted, sometimes not
 
-		this.documents = new Documents({ id: properties?.id });
-	}
+        this.documents = new Documents({ id: properties?.id });
+    }
 
-	async open() {
-		// this will be invoked on startup
-		await this.documents.open({ type: TextDocument, index: { idProperty: "id" } });
-	}
+    async open() {
+        // this will be invoked on startup
+        await this.documents.open({
+            type: TextDocument,
+            index: { idProperty: "id" },
+        });
+    }
 }
